a0e112200c68c5a9011ff340032a565e2d370027
rebeccareitzammercastillo <rebeccareitzammer@yahoo.com>
2020-09-15 22:39:54 -0500

    css/responsive/bug fixes

--- login_registration_app/urls.py
+++ login_registration_app/urls.py
@@ -1,12 +1,22 @@
 from django.urls import path     
 from . import views
+
+app_name = 'search'
+
 urlpatterns = [
     path('', views.index),
     path('register', views.register),
     path('newaccount', views.newaccount),
     path('newsfeed', views.newsfeed),
+    path('profile/<profileid>', views.profile),
     path('logout', views.logout),
     path('login', views.login),
     path('postmessage', views.postmessage),
-    path('postcomment/<messageid>', views.postcomment)      
+    path('postcomment/<messageid>', views.postcomment),    
+    path('delete/<postid>', views.deletepost),
+    path('deletecomment/<commentid>', views.deletecomment),
+    path('profilemessage/<profileid>', views.profilemessage),
+    path('profile/<profileid>/comment/<commentid>', views.profilecomment),    
+    path('profile/<profileid>/deleteprofilepost/<postid>', views.deleteprofilepost),
+    path('profile/<profileid>/deleteprofilecomment/<commentid>', views.deleteprofilecomment),
 ]
--- login_registration_app/views.py
+++ login_registration_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from .models import Register, Message, Comment
 from django.contrib import messages
 import bcrypt
@@ -12,7 +13,7 @@ def register(request):
     if len(validationErrors) > 0:
         for key, value in validationErrors.items():
             messages.error(request, value)
-        return redirect('/register')
+        return redirect('/newaccount')
     hashPW = bcrypt.hashpw(request.POST['pw'].encode(), bcrypt.gensalt()).decode()
     newuser = Register.objects.create(first_name= request.POST['fname'], last_name= request.POST ['lname'], email= request.POST ['email'], password= hashPW)
     request.session['loggedinID'] = newuser.id
@@ -44,9 +45,28 @@ def newsfeed(request):
         'allmessages': Message.objects.all().order_by("-created_at"),
         'allcomments': Comment.objects.all()
     }
+
+    query=""
+    if request.GET:
+        query= request.GET['q']
+        context['query'] = str(query)
+
 #     ClassName.objects.all().order_by("field_name") - orders by field provided, ascending
-# ClassName.objects.all().order_by("-field_name")
+#      ClassName.objects.all().order_by("-field_name")
+
+
     return render(request,'success.html', context)
+
+def profile(request, profileid):
+    if 'loggedinID' not in request.session:
+        return redirect('/')
+    context= {
+        'loggedinUser': Register.objects.get(id= request.session['loggedinID']),
+        'profiletoshow': Register.objects.get(id=profileid),
+        'myposts' : Message.objects.filter(message_uploader= Register.objects.get(id= profileid)),
+        'allcomments': Comment.objects.all()
+    }
+    return render(request,'profile.html', context)
 
 def postmessage(request):
     newmessage = Message.objects.create(message= request.POST['postmessage'], message_uploader = Register.objects.get(id=request.session['loggedinID']))
@@ -57,3 +77,58 @@ def postcomment(request, messageid):
     # print(Comment.objects.all())
     # print(messageid)
     return redirect('/newsfeed')
+
+def deletepost(request, postid):
+    deletepost=Message.objects.get(id=postid)
+    deletepost.delete()
+    return redirect('/newsfeed')
+    
+
+def deletecomment(request, commentid):
+    deletecomment=Comment.objects.get(id=commentid)
+    deletecomment.delete()
+    return redirect('/newsfeed')
+
+def profilemessage(request, profileid):
+    newmessage = Message.objects.create(message= request.POST['postmessage'], message_uploader = Register.objects.get(id=request.session['loggedinID']))
+    return redirect(f'/profile/{profileid}')
+
+def profilecomment(request, profileid, commentid):
+    newcomment = Comment.objects.create(comment= request.POST['postcomment'], comment_uploader = Register.objects.get(id=request.session['loggedinID']), message_comment=Message.objects.get(id=commentid))
+    # print(Comment.objects.all())
+    # print(messageid)
+    return redirect(f'/profile/{profileid}')
+
+def deleteprofilepost(request, profileid, postid):
+    deletepost=Message.objects.get(id=postid)
+    deletepost.delete()
+    return redirect(f'/profile/{profileid}')
+    
+
+def deleteprofilecomment(request, profileid, commentid):
+    deletecomment=Comment.objects.get(id=commentid)
+    deletecomment.delete()
+    return redirect(f'/profile/{profileid}')
+
+def get_user_queryset(query=None):
+    queryset=[]
+    queries= query.split(" ")
+    for q in queries:
+        users= Register.objects.filter(
+            Q(first_name__icontains=q),
+            Q(last_name__icontains=q)
+        ).distinct()
+        
+        for user in users:
+            queryset.append(user)
+    
+    return list(set(queryset))
+
+# def detail_user_view(request, slug):
+
+# 	context = {}
+
+# 	User = get_object_or_404(User, slug=slug)
+# 	context['user'] = user
+
+# 	return render(request, '/detail_user.html', context)
